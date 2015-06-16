35473005264c72a15dca45a408b8ea12eb440582
Maria Grynychyn <mgrynychyn@Marias-MacBook-Pro.local>
2015-06-16 14:48:15 -0400

    Initial Commit

---
+++ TransferNowM/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TransferNowM
+//
+//  Created by Maria Grynychyn on 6/16/15.
+//  Copyright (c) 2015 Maria Grynychyn. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
---
+++ TransferNowM/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  TransferNowM
+//
+//  Created by Maria Grynychyn on 6/16/15.
+//  Copyright (c) 2015 Maria Grynychyn. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
